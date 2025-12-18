6fbb8c39d0d247bc56883f7eafa8c4edbb8c98a4
Aswun <aswinlutfian32@gmail.com>
2025-12-18 14:07:36 +0700

    mematikan comment pada RepoDataSiswa.kt dan ServiceApiSiswa.kt

--- app/src/main/java/com/example/prak12/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/prak12/apiservice/ServiceApiSiswa.kt
@@ -15,12 +15,12 @@ interface ServiceApiSiswa {
 
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
-//    @GET("baca1Teman.php/{id}")
-//    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
-//
-//    @PUT("editTM.php/{id}")
-//    suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa):retrofit2.Response<Void>
-//
-//    @DELETE("hapusTM.php/{id}")
-//    suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
+    @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @DELETE("hapusTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
 }
--- app/src/main/java/com/example/prak12/repo/RepoDataSiswa.kt
+++ app/src/main/java/com/example/prak12/repo/RepoDataSiswa.kt
@@ -6,9 +6,9 @@ import com.example.prak12.modeldata.DataSiswa
 interface RepoDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
-//    suspend fun getSatuSiswa(id: Int): DataSiswa
-//    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
-//    suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void>
 }
 
 class JaringanRepoDataSiswa(
@@ -16,7 +16,7 @@ class JaringanRepoDataSiswa(
 ) : RepoDataSiswa {
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
-//    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa()
-//    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.editSatuSiswa(id, dataSiswa)
-//    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa()
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
 }
