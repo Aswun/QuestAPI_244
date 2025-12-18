059cd57caabf60414e2a01dcbd2d36ebb3e977f8
Aswun <aswinlutfian32@gmail.com>
2025-12-18 14:08:25 +0700

    Memperbaiki pemanggilan fungsi getSatuSiswa dengan menambahkan parameter id

--- app/src/main/java/com/example/prak12/repo/RepoDataSiswa.kt
+++ app/src/main/java/com/example/prak12/repo/RepoDataSiswa.kt
@@ -16,7 +16,7 @@ class JaringanRepoDataSiswa(
 ) : RepoDataSiswa {
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
-    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa()
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
     override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.editSatuSiswa(id, dataSiswa)
     override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
 }
