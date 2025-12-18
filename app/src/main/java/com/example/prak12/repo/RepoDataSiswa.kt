81f5076cbac806080988c31a7008d8fbf4e7c235
Aswun <aswinlutfian32@gmail.com>
2025-12-18 13:39:22 +0700

    Membuat RepoDataSiswa.kt pada package repo

---
+++ app/src/main/java/com/example/prak12/repo/RepoDataSiswa.kt
@@ -0,0 +1,22 @@
+package com.example.prak12.repo
+
+import com.example.prak12.apiservice.ServiceApiSiswa
+import com.example.prak12.modeldata.DataSiswa
+
+interface RepoDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+//    suspend fun getSatuSiswa(id: Int): DataSiswa
+//    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
+//    suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void>
+}
+
+class JaringanRepoDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepoDataSiswa {
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+//    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa()
+//    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+//    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
+}
